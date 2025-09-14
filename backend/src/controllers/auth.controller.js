6f767c79cc1ef6bce932fd61fdb214d98cf334d5
kevinsultana <kevinpdg.ks@gmail.com>
2025-09-14 22:49:56 +0700

    feat: preparing to use the auth api

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const register = (req, res) => {
+  // Registration logic here
+  res.send("User registered");
+};
+
+export const login = (req, res) => {
+  // Login logic here
+  res.send("User logged in");
+};
+
+export const logout = (req, res) => {
+  // Logout logic here
+  res.send("User logged out");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import connectDB from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  //   connectDB();
+});
