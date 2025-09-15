e99f18999aa24a97f612b57157d962863c5bb39a
kevinsultana <kevinpdg.ks@gmail.com>
2025-09-15 22:54:49 +0700

    all function without socket io completed

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,63 @@
-export const getAllConnectedUsers = (req, res) => {
-  // Logic to get all connected users
-  res.status(200).json({ message: "List of all connected users", users: [] });
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getAllConnectedUsers = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res
+      .status(200)
+      .json({ message: "List of all connected users", users: filteredUsers });
+  } catch (error) {
+    console.error("Error fetching connected users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatWith } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, recipientId: userToChatWith },
+        { senderId: userToChatWith, recipientId: myId },
+      ],
+    }).sort({ createdAt: 1 });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { id: recipientId } = req.params;
+    const senderId = req.user._id;
+    const { text, image } = req.body;
+
+    let imageUrl = null;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      recipientId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+    // todo realtime function with socket.io
+    res.status(201).json({ message: "Message sent successfully", newMessage });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getAllConnectedUsers } from "../controllers/message.controller.js";
+import {
+  getAllConnectedUsers,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getAllConnectedUsers);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
