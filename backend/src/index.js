00a03c9f17f3ec9ca30fc78a9d185a358492becd
kevinsultana <kevinpdg.ks@gmail.com>
2025-09-22 23:11:53 +0700

    fix: bug on deployin on render

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,10 +33,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
